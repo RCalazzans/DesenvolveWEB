4d21032bf6a772d1dccc49fb560c1224711ff6b4
renan.ecteixeira <renan.ecteixeira@BOT00DM8Q6W11-1>
2025-06-11 11:07:58 -0300

    11/06 PHP

---
+++ PHP/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo</title>
+</head>
+
+<body>
+    <?php
+    # primeira linha
+    //echo "Ola mundo";
+    //Imprime a mensagem ola mundo na tela
+    /* Comentarios em linha podem ser feitos com os simbolos # e //, ja os comentarios com mais uma linha devem ser feitos com os sinais /* e */
+    //phpinfo();
+
+    //Variaveis em PHP
+
+    $texto = "<h1>Sou aluno da TI36 no Senac Botucatu</h1>";
+    echo $texto;
+
+    $divisao = "<br>=========================================================<br>";
+    echo $divisao;
+    $nome = "Chaves";
+    $sobrenome = "del Ocho";
+    echo "Ola, Meu nome é $nome e meu sobrenome é $sobrenome.";
+    ?>
+</body>
+
+</html>
